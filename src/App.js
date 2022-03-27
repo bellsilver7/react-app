d22ece7fcdfb31eb981ee7f0ce7909b3da19490d
bellsilver7 <bellsilver7@gmail.com>
2022-03-27 22:13:27 +0900

    컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return <header>
+    <h1><a href="/">Web</a></h1>
+  </header>
+}
+
+function Nav()
+{
+  return <nav>
+    <ol>
+      <li><a href="/read/1">Html</a></li>
+      <li><a href="/read/2">Css</a></li>
+      <li><a href="/read/3">Javascript</a></li>
+    </ol>
+  </nav>
+}
+
+function Article()
+{
+  return <article>
+    <h2>Welcome</h2>
+    Hello React!
+  </article>
+}
+
 function App() {
   return (
-    <div className="App">
-      Hello React!
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
