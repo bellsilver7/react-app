270b6edfd75a9e0f344d5cad1cd07cc0c7c30040
bellsilver7 <bellsilver7@gmail.com>
2022-03-28 20:41:53 +0900

    컴포넌트에 PROP(속성) 부여

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header() {
+function Header(props) {
   return <header>
-    <h1><a href="/">Web</a></h1>
+    <h1><a href="/">{props.title}</a></h1>
   </header>
 }
 
-function Nav()
+function Nav(props)
 {
+  const lis = [];
+  props.topics.map((t) => {
+    // 동적으로 만들어주는 태그는 key라는 prop을 가져야 한다.
+    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>);
+  });
   return <nav>
     <ol>
-      <li><a href="/read/1">Html</a></li>
-      <li><a href="/read/2">Css</a></li>
-      <li><a href="/read/3">Javascript</a></li>
+      {lis}
     </ol>
   </nav>
 }
 
-function Article()
+function Article(props)
 {
   return <article>
-    <h2>Welcome</h2>
-    Hello React!
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 
 function App() {
+  // 변하지 않는 변수는 const로 선언하여 코드를 튼튼하게 할 수 있다.
+  const topics = [
+    {id: 1, title: "html", body: "html is ..."},
+    {id: 2, title: "css", body: "css is ..."},
+    {id: 3, title: "javascript", body: "javascript is ..."}
+  ];
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="REACT"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello React!"></Article>
     </div>
   );
 }
